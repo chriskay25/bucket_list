971c28de39276019025992c50964de78f3b5a0a2
Chris Kay <ckay84@protonmail.com>
2020-06-26 10:13:15 -0400

    Added user validations, labels for login and signup forms, seed data, links for user's show page.

---
+++ db/seeds.rb
@@ -0,0 +1,5 @@
+User.create(username: "Todd", password: "Gaston")
+User.create(username: "Doctor", password: "Phil")
+User.create(username: "Chuck", password: "Norris")
+User.create(username: "Philip", password: "Roth")
+User.create(username: "Tinker", password: "Tailor")
