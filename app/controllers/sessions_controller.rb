4bb62f2155ab027c3fce072e431ea4fdf563719b
Chris Kay <ckay84@protonmail.com>
2020-06-25 23:05:05 -0400

    Login routes for sessions_controller and login view.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -14,4 +14,18 @@ class SessionsController < ApplicationController
     end 
   end 
 
+  get '/login' do
+    erb :login
+  end 
+
+  post '/login' do
+    user = User.find_by(username: params[:username])
+    if user && user.authenticate(params[:password])
+      session[:user_id] = user.id
+      redirect "/users/#{user.id}"
+    else
+      redirect '/'
+    end 
+  end 
+
 end 
