bc6566f73941b035b9f8f5f5ee0ba85920ed5168
Chris Kay <ckay84@protonmail.com>
2020-06-28 17:02:57 -0400

    Added destinations migration, class, and associations for destination and user models.

---
+++ app/models/destination.rb
@@ -0,0 +1,4 @@
+class Destinations < ActiveRecord::Base
+  has_and_belongs_to_many :users
+
+end 
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_25_171800) do
+ActiveRecord::Schema.define(version: 2020_06_28_204827) do
+
+  create_table "destinations", force: :cascade do |t|
+    t.string "country"
+    t.string "city"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "username"
