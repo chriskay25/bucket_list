382981c6a1896122efee0f6912201c89605f28c7
Chris Kay <ckay84@protonmail.com>
2020-06-29 00:10:09 -0400

    Made destinations_users join table.

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_28_204827) do
+ActiveRecord::Schema.define(version: 2020_06_28_210618) do
 
   create_table "destinations", force: :cascade do |t|
     t.string "country"
     t.string "city"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "destinations_users", id: false, force: :cascade do |t|
+    t.integer "user_id", null: false
+    t.integer "destination_id", null: false
+    t.index ["destination_id"], name: "index_destinations_users_on_destination_id"
+    t.index ["user_id"], name: "index_destinations_users_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
