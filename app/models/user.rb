c13c7fca6b368fa4c9bc71b88027d7209ec497a7
Chris Kay <49329134+chriskay25@users.noreply.github.com>
2021-08-02 19:19:36 -0400

    Delete references to now deleted experiences

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,15 +1,10 @@
 class User < ActiveRecord::Base
   has_secure_password
   has_many :destinations, dependent: :destroy
-  has_many :experiences, dependent: :destroy
   validates :username, presence: true, uniqueness: true
   
   def dest_count
     destinations.count
-  end 
-
-  def exp_count
-    experiences.count
   end 
 
 end 
